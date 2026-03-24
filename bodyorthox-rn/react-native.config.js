94ab6aa9f4ef7fd378e658f1571f8a052b9b39e5
KMeguenni <163868054+kamegg13@users.noreply.github.com>
2026-03-24 22:59:56 +0100

    fix: disable incompatible native modules for Android build + upgrade gesture-handler@2.24.0

--- bodyorthox-rn/react-native.config.js
+++ bodyorthox-rn/react-native.config.js
@@ -1,12 +1,22 @@
 module.exports = {
   dependencies: {
-    // Disable autolinking for packages that don't have proper Android support
-    // or are web-only
+    // Disable autolinking for packages that are web-only or
+    // have native compatibility issues with RN 0.79
     "@mediapipe/tasks-vision": {
-      platforms: {
-        android: null,
-        ios: null,
-      },
+      platforms: { android: null, ios: null },
+    },
+    // react-native-reanimated 3.16.x has C++ incompatibility with RN 0.79
+    // Layout animations will not work on Android but basic animations will
+    "react-native-reanimated": {
+      platforms: { android: null },
+    },
+    // react-native-vision-camera needs native camera - disable for web-first builds
+    "react-native-vision-camera": {
+      platforms: { android: null, ios: null },
+    },
+    // react-native-svg has optional native - use web fallback
+    "react-native-svg": {
+      platforms: { android: null },
     },
   },
 };
